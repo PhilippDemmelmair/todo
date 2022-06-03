2ad20196fa606f0a1f3f33c0978a586f850fdcd0
Donnie-design <56469697+Donnie-design@users.noreply.github.com>
2022-06-03 11:37:55 +0200

    add routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,13 @@ import { useState } from 'react'
 import styled from 'styled-components'
 import { nanoid } from 'nanoid'
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes,
+} from 'react-router-dom'
 
 import { Todo } from '../TaskObjects/Todo'
 import { Add } from '../TaskObjects/Add'
@@ -17,8 +24,11 @@ const defaultTask = [
 
 function App() {
   const [todoList, setTodo] = useState(
-    // TODO store in localStorage
     // TODO find better colours
+    // TODO move this to global state to get it out from here, CustomHook
+    // TODO Refactoring
+    // TODO ReadMe anpassen
+    // TODO MobileView
 
     JSON.parse(localStorage.getItem('todoList')) || defaultTask
   )
@@ -62,11 +72,29 @@ function App() {
     localStorage.setItem('todoList', JSON.stringify(todoList))
   }, [todoList])
 
-  console.table(todoList)
+  // console.table(todoList)
   return (
     <>
       <Header>
         <Title>SUPER IMPORTANT TODO APP</Title>
+        <Router>
+          <NavBar>
+            <NavItem>
+              <Link to="/">Home</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/random">Random</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/archive">Archive</Link>
+            </NavItem>
+          </NavBar>
+          <Routes>
+            <Route path="/random" element={<Random />}></Route>
+            <Route path="/archive" element={<Archive />}></Route>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </Router>
       </Header>
       <Body>
         <Add addTodo={addTodo} />
@@ -88,13 +116,27 @@ function App() {
   )
 }
 
+function Home() {
+  return <h2>Home</h2>
+}
+
+function Archive() {
+  return <h2>Archive</h2>
+}
+
+function Random() {
+  return <h2>Random</h2>
+}
+
+// TODO auslagern
+
 const Header = styled.header`
   background-color: #282c34;
   height: 10vh;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
   border-bottom: 1px solid #fff;
 `
 
@@ -109,4 +151,21 @@ const Body = styled.body`
   padding: 10vw;
 `
 
+const NavItem = styled.button`
+  background: #fff;
+  border-radius: 8px;
+  padding: 10px 20px;
+  color: #fff;
+  background: transparent;
+  border: 2px solid #fff;
+`
+const NavBar = styled.nav`
+  border: 1px solid #a1a1a1;
+  border-radius: 8px;
+  width: 30vw;
+  padding: 1vh;
+  display: flex;
+  flex-flow: row;
+  justify-content: space-between;
+`
 export default App
