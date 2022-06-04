851a91ccfdebcb0279fef987cafad0e504a1299c
Donnie-design <56469697+Donnie-design@users.noreply.github.com>
2022-06-04 20:52:36 +0200

    fixed random page

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,7 +1,31 @@
+import useTodo from './../common/useTodo'
+import shallow from 'zustand/shallow'
+
+import { Body } from './styledComponents'
+import { Todo } from './TaskObjects/Todo'
+
 function Random() {
+  const todoIds = useTodo(
+    (state) =>
+      state.todos.filter((todo) => !todo.archived).map((todo) => todo.id),
+    shallow
+  )
+  console.log(todoIds.length)
+  const defaultMessage = 'You have absolutly nothing to do!'
+
   return (
     <>
-      <h2>Do this - NOW!!!</h2>
+      <Body>
+        <h2>Do this - NOW!!!</h2>
+        {todoIds.length > 0 ? (
+          <Todo
+            key={todoIds[Math.floor(Math.random() * (todoIds.length - 1))]}
+            id={todoIds[Math.floor(Math.random() * (todoIds.length - 1))]}
+          />
+        ) : (
+          <p>{defaultMessage}</p>
+        )}
+      </Body>
     </>
   )
 }
