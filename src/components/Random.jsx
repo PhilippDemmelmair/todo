eac8cd2846241ea7893997b31ce433e7b6c72126
Donnie-design <56469697+Donnie-design@users.noreply.github.com>
2022-06-05 01:05:53 +0200

    add shuffle button on random page

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,8 +1,12 @@
 import useTodo from './../common/useTodo'
 import shallow from 'zustand/shallow'
+import styled from 'styled-components'
+import { useEffect } from 'react'
 
 import { Body } from './styledComponents'
 import { Todo } from './TaskObjects/Todo'
+import { useState } from 'react'
+import React from 'react'
 
 function Random() {
   const todoIds = useTodo(
@@ -10,18 +14,25 @@ function Random() {
       state.todos.filter((todo) => !todo.archived).map((todo) => todo.id),
     shallow
   )
-  console.log(todoIds.length)
+
   const defaultMessage = 'You have absolutly nothing to do!'
+
+  const [random, setRandom] = useState(
+    todoIds[Math.floor(Math.random() * (todoIds.length - 1))]
+  )
+
+  function getRandom() {
+    console.log('Changed')
+    setRandom(todoIds[Math.floor(Math.random() * (todoIds.length - 1))])
+  }
 
   return (
     <>
       <Body>
         <h2>Do this - NOW!!!</h2>
+        <Button onClick={() => getRandom()}>Shuffle</Button>
         {todoIds.length > 0 ? (
-          <Todo
-            key={todoIds[Math.floor(Math.random() * (todoIds.length - 1))]}
-            id={todoIds[Math.floor(Math.random() * (todoIds.length - 1))]}
-          />
+          <Todo key={random} id={random} />
         ) : (
           <p>{defaultMessage}</p>
         )}
@@ -31,3 +42,12 @@ function Random() {
 }
 
 export { Random }
+
+const Button = styled.button`
+  background: transparent;
+  color: #fff;
+  padding: 10px 20px;
+  width: 15%;
+  border: 2px solid #fff;
+  border-radius: 8px;
+`
