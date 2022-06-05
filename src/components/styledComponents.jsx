aaad65f1e6a4fc68bf345d5b854feea18b58b008
Donnie-design <56469697+Donnie-design@users.noreply.github.com>
2022-06-05 19:11:38 +0200

    redesign body

--- src/components/styledComponents.jsx
+++ src/components/styledComponents.jsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
 const Body = styled.body`
-  height: 90vh;
+  min-height: 90vh;
   background: #282c34;
-  padding: 10vw;
+  padding: 1vw 10vw;
 `
 
 export { Body }
